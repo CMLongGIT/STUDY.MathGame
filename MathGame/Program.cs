b97aaaed381087202041204862c763b81be92dbc
Cole Long <coleml16@gmail.com>
2023-06-08 14:11:32 -0500

    Changed starting text, and added game select logic.

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -4,7 +4,7 @@ var name = Console.ReadLine();
 var date = DateTime.UtcNow;
 
 Console.WriteLine("-----------------------------------------------");
-Console.WriteLine($"Hello {name}. It's {date}. This is your math game, and it's also proof that you know the basics of C#. \n");
+Console.WriteLine($"Hello {name}. It's {date}. This is a simple collection of C# math games to learn fundamentals. \n");
 Console.WriteLine($@"What game would you like to play today? Choose from the options below:
 A - Addition
 S - Subtraction
@@ -12,3 +12,31 @@ M - Multiplication
 D - Division
 Q - Quit the program");
 Console.WriteLine("-----------------------------------------------");
+
+var gameSelected = Console.ReadLine();
+
+if (gameSelected.Trim().ToLower() == "a")
+{
+    Console.WriteLine("Addition game selected.");
+}
+else if (gameSelected.Trim().ToLower() == "s")
+{
+    Console.WriteLine("Subtraction game selected.");
+}
+else if (gameSelected.Trim().ToLower() == "m")
+{
+    Console.WriteLine("Multiplication game selected.");
+}
+else if (gameSelected.Trim().ToLower() == "d")
+{
+    Console.WriteLine("Division game selected.");
+}
+else if (gameSelected.Trim().ToLower() == "q")
+{
+    Console.WriteLine("Goodbye");
+    Environment.Exit(1);
+}
+else
+{
+    Console.WriteLine("Invalid Input.");
+}
