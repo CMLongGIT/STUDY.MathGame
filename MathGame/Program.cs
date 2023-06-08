5fe7a74d7e9607f2a09947937fcfc1a92a8af195
Cole Long <coleml16@gmail.com>
2023-06-08 13:51:34 -0500

    Add project files.

---
+++ MathGame/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Please type your name.");
+
+var name = Console.ReadLine();
+var date = DateTime.UtcNow;
+
+Console.WriteLine("-----------------------------------------------");
+Console.WriteLine($"Hello {name}. It's {date}. This is your math game, and it's also proof that you know the basics of C#. \n");
+Console.WriteLine($@"What game would you like to play today? Choose from the options below:
+A - Addition
+S - Subtraction
+M - Multiplication
+D - Division
+Q - Quit the program");
+Console.WriteLine("-----------------------------------------------");
